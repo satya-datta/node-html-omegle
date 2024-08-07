012c746fa56aee0fd64f8d7be003b2f2865a8897
Satya <satyadattakallepalli@gmail.com>
2024-08-07 18:49:32 +0530

    change 2

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  socket.on('join', ({ username, tags }) => {
-    socket.username = username;
+  socket.on('join', (tags) => {
     socket.tags = tags;
+    socket.chatting = false;
 
     let matched = false;
     for (let i = 0; i < waitingUsers.length; i++) {
       if (waitingUsers[i].tags.some(tag => tags.includes(tag))) {
         const matchedUser = waitingUsers.splice(i, 1)[0];
-        const room = `${socket.id}#${matchedUser.socket.id}`;
+        const room = ${socket.id}#${matchedUser.socket.id};
 
         socket.join(room);
         matchedUser.socket.join(room);
@@ -30,8 +30,8 @@ io.on('connection', (socket) => {
         socket.room = room;
         matchedUser.socket.room = room;
 
-        socket.emit('matched', { username: matchedUser.socket.username });
-        matchedUser.socket.emit('matched', { username });
+        socket.emit('matched', { type: matchedUser.socket.type });
+        matchedUser.socket.emit('matched', { type: socket.type });
 
         matched = true;
         break;
@@ -39,34 +39,59 @@ io.on('connection', (socket) => {
     }
 
     if (!matched) {
-      waitingUsers.push({ socket, username, tags });
+      waitingUsers.push({ socket, tags, type: socket.type });
     }
   });
 
+  socket.on('setType', (type) => {
+    socket.type = type;
+  });
+
   socket.on('chat message', (msg) => {
-    // Emit the message to the other user in the room
     socket.to(socket.room).emit('chat message', { sender: 'stranger', message: msg.message });
-    // Only emit the message to the sender directly (without using rooms)
     socket.emit('self message', { sender: 'you', message: msg.message });
   });
 
-  socket.on('offer', (offer) => {
-    socket.to(socket.room).emit('offer', offer);
-  });
-
-  socket.on('answer', (answer) => {
-    socket.to(socket.room).emit('answer', answer);
-  });
-
-  socket.on('ice-candidate', (candidate) => {
-    socket.to(socket.room).emit('ice-candidate', candidate);
+  socket.on('skip', () => {
+    socket.leave(socket.room);
+    socket.to(socket.room).emit('strangerDisconnected');
+    waitingUsers = waitingUsers.filter(user => user.socket !== socket);
+    socket.emit('finding');
+    joinQueue(socket);
   });
 
   socket.on('disconnect', () => {
     waitingUsers = waitingUsers.filter(user => user.socket !== socket);
+    socket.to(socket.room).emit('strangerDisconnected');
     console.log('User disconnected');
   });
 });
+
+function joinQueue(socket) {
+  let matched = false;
+  for (let i = 0; i < waitingUsers.length; i++) {
+    if (waitingUsers[i].tags.some(tag => socket.tags.includes(tag))) {
+      const matchedUser = waitingUsers.splice(i, 1)[0];
+      const room = ${socket.id}#${matchedUser.socket.id};
+
+      socket.join(room);
+      matchedUser.socket.join(room);
+
+      socket.room = room;
+      matchedUser.socket.room = room;
+
+      socket.emit('matched', { type: matchedUser.socket.type });
+      matchedUser.socket.emit('matched', { type: socket.type });
+
+      matched = true;
+      break;
+    }
+  }
+
+  if (!matched) {
+    waitingUsers.push({ socket, tags: socket.tags, type: socket.type });
+  }
+}
 
 server.listen(3000, () => {
   console.log('Server listening on port 3000');
